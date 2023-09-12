6253b871956bc94334a6002cfc994c7f4afa67a1
Pedro Lizarraga <plizarraga86@gmail.com>
2023-09-12 12:27:45 -0700

    added websockets to the client app

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
-
+import { Socket } from 'ngx-socket-io';
+export interface Event {
+  id: number;
+  deviceId: string;
+  eventType: string;
+  batteryLevel: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +15,22 @@ import { Component, OnInit, inject } from '@angular/core';
 export class AppComponent implements OnInit {
   private http = inject(HttpClient);
   title = 'client';
+  events: Event[] = [];
+
+  constructor(private socket: Socket) {}
 
   ngOnInit(): void {
-    this.http.get('/api').subscribe((res) => console.log(res));
+    this.http.get<Event[]>('/api/events').subscribe((res) => {
+      console.log(res);
+      this.events = res;
+    });
+    this.getCreatedEvent();
+  }
+
+  getCreatedEvent() {
+    return this.socket.fromEvent<Event>('event_created').subscribe((data) => {
+      console.log(data);
+      this.events.unshift(data);
+    });
   }
 }
