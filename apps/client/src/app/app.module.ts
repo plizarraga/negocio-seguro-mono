edb75f716a4f9d4debb820791beac1fcb83568f5
Pedro Lizarraga <plizarraga86@gmail.com>
2023-09-12 13:37:09 -0700

    Changed URI for websockets in the client

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: '/api', options: {} };
 @NgModule({
   declarations: [AppComponent],
   imports: [
