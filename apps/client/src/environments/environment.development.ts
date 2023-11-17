afd52bcfa0d72be6d66841e20cdc7ce836200948
Pedro Lizarraga <plizarraga86@gmail.com>
2023-11-16 18:22:50 -0800

    env endpoint for prod in dev mode

--- apps/client/src/environments/environment.development.ts
+++ apps/client/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  webSocketUrl: 'http://localhost:3000',
+  // webSocketUrl: 'http://localhost:3000',
+  webSocketUrl: 'https://negocio-seguro-app.up.railway.app/',
 };
