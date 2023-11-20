3f4ff06124f7b66147397b47a76ef219c5b9471d
Pedro Lizarraga <plizarraga86@gmail.com>
2023-11-20 10:19:43 -0800

    added prod websocker url

--- apps/client/src/environments/environment.ts
+++ apps/client/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  webSocketUrl: 'http://localhost:3000',
-  // webSocketUrl: 'https://negocio-seguro-app.up.railway.app/',
+  webSocketUrl: 'https://negocio-seguro-app.up.railway.app/',
 };
